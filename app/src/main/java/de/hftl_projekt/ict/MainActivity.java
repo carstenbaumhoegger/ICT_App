57692e05faeae77697efe177a8b03f0471a81d39
Carsten <cbaumhoegger@tambourcorps-app.de>
2015-03-04 12:53:47 +0100

    implemented a simple preview that shows the current camera picture added the needed permissions in manifest

--- app/src/main/java/de/hftl_projekt/ict/MainActivity.java
+++ app/src/main/java/de/hftl_projekt/ict/MainActivity.java
@@ -1,14 +1,60 @@
 package de.hftl_projekt.ict;
 
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.FrameLayout;
 
 
 public class MainActivity extends ActionBarActivity {
+    public static final String TAG = "MainActivity";
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+            e.printStackTrace();
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    /**
+     * release camera for other applications
+     */
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //release the camera if the app is closed
+        releaseCamera();
+        Log.d(TAG, "onDestroy");
     }
 }
