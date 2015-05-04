e8e4d806e546c7a5bae38df9f13e4d56d0fe4fb6
carstenbaumhoegger <cbaumhoegger@tambourcorps-app.de>
2015-05-04 18:59:22 +0200

    implemented Butterknife library for view "injection"

--- app/src/main/java/de/hftl_projekt/ict/MainActivity.java
+++ app/src/main/java/de/hftl_projekt/ict/MainActivity.java
@@ -2,28 +2,32 @@ package de.hftl_projekt.ict;
 
 import android.hardware.Camera;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.FrameLayout;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
 
     private Camera mCamera;
-    private CameraPreview mPreview;
+
+    @InjectView(R.id.camera_preview)
+    FrameLayout preview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.inject(this);
         // Create an instance of Camera
         mCamera = getCameraInstance();
 
         // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        CameraPreview mPreview = new CameraPreview(this, mCamera);
         preview.addView(mPreview);
     }
 
