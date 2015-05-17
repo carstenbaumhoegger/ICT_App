d223cae335ea357b426482986aa06ddd1904b6b9
carstenbaumhoegger <cbaumhoegger@tambourcorps-app.de>
2015-05-17 21:45:34 +0200

    implemented methods from OpenCVDemo

--- app/src/main/java/de/hftl_projekt/ict/MainActivity.java
+++ app/src/main/java/de/hftl_projekt/ict/MainActivity.java
@@ -1,57 +1,78 @@
 package de.hftl_projekt.ict;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
+import android.view.MotionEvent;
+
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import butterknife.InjectView;
 import butterknife.OnClick;
 import de.hftl_projekt.ict.base.BaseActivity;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements CameraBridgeViewBase.CvCameraViewListener {
     public static final String TAG = "MainActivity";
 
-    private Camera mCamera;
+    /** opencv camera view class */
+    @InjectView(R.id.camera_view) JavaCameraView mOpenCvCameraView;
 
-    @InjectView(R.id.camera_preview) FrameLayout preview;
+    private boolean bShootNow = false, bDisplayTitle = true;
+
+    private byte[] byteColourTrackCentreHue;
+
+    private double dTextScaleFactor;
+
+    private int  iFileOrdinal = 0;
+
+    private List<Integer> iHueMap, channels;
+    private List<Float> ranges;
+
+    private long lFrameCount = 0, lMilliStart = 0, lMilliNow = 0, lMilliShotTime = 0;
+
+    private Mat mRgba, mGray, mIntermediateMat, mMatRed, mMatGreen, mMatBlue, mROIMat,
+            mMatRedInv, mMatGreenInv, mMatBlueInv, mHSVMat, mErodeKernel, mContours,
+            lines;
+
+    private MatOfRect faces;
+    private MatOfPoint2f mMOP2f1, mMOP2f2;
+    private MatOfPoint2f mApproxContour;
+    private MatOfPoint MOPcorners;
+
+    private Scalar colorRed, colorGreen;
+    private Size sSize3, sMatSize;
+    private String string, sShotText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
 
-        // Create our Preview view and set it as the content of our activity.
-        CameraPreview mPreview = new CameraPreview(this, mCamera);
-        preview.addView(mPreview);
-    }
+        mOpenCvCameraView.setCvCameraViewListener(this);
 
-    /** A safe way to get an instance of the Camera object. */
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-            e.printStackTrace();
-        }
-        return c; // returns null if camera is unavailable
-    }
-
-    /**
-     * release camera for other applications
-     */
-    private void releaseCamera(){
-        if (mCamera != null){
-            mCamera.release();
-            mCamera = null;
-        }
     }
 
     /**
@@ -64,8 +85,8 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        //release the camera if the app is closed
-        releaseCamera();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
         Log.d(TAG, "onDestroy");
     }
 
@@ -91,5 +112,176 @@ public class MainActivity extends BaseActivity {
     @Override
     protected int getLayoutResourceId() {
         return R.layout.activity_main;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        byteColourTrackCentreHue = new byte[3];
+        // green = 60 // mid yellow  27
+        byteColourTrackCentreHue[0] = 27;
+        byteColourTrackCentreHue[1] = 100;
+        byteColourTrackCentreHue[2] = (byte)255;
+
+        channels = new ArrayList<>();
+        channels.add(0);
+        colorRed = new Scalar(255, 0, 0, 255);
+        colorGreen = new Scalar(0, 255, 0, 255);
+
+        faces = new MatOfRect();
+
+        iHueMap = new ArrayList<>();
+        iHueMap.add(0);
+        iHueMap.add(0);
+        lines = new Mat();
+
+        mApproxContour = new MatOfPoint2f();
+        mContours = new Mat();
+        mGray = new Mat();
+        mHSVMat = new Mat();
+        mIntermediateMat = new Mat();
+        mMatRed = new Mat();
+        mMatGreen = new Mat();
+        mMatBlue = new Mat();
+        mMatRedInv = new Mat();
+        mMatGreenInv = new Mat();
+        mMatBlueInv = new Mat();
+        mMOP2f1 = new MatOfPoint2f();
+        mMOP2f2 = new MatOfPoint2f();
+        MOPcorners = new MatOfPoint();
+        mRgba = new Mat();
+        mROIMat = new Mat();
+
+        ranges = new ArrayList<>();
+        ranges.add(50.0f);
+        ranges.add(256.0f);
+
+        sMatSize = new Size();
+        sSize3 = new Size(3, 3);
+
+        string = "";
+
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        int densityDpi = dm.densityDpi;
+        dTextScaleFactor = ((double)densityDpi / 240.0) * 0.9;
+
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mIntermediateMat = new Mat(height, width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        releaseMats();
+    }
+
+    public void releaseMats () {
+        mRgba.release();
+        mIntermediateMat.release();
+        mGray.release();
+        mMatRed.release();
+        mMatGreen.release();
+        mMatBlue.release();
+        mROIMat.release();
+        mMatRedInv.release();
+        mMatGreenInv.release();
+        mMatBlueInv.release();
+        mHSVMat.release();
+        mErodeKernel.release();
+        mContours.release();
+        lines.release();
+        faces.release();
+        MOPcorners.release();
+        mMOP2f1.release();
+        mMOP2f2.release();
+        mApproxContour.release();
+
+    }
+
+    @Override
+    public Mat onCameraFrame(Mat inputFrame) {
+        mErodeKernel = Imgproc.getStructuringElement(Imgproc.MORPH_CROSS, sSize3);
+
+        // start the timing counter to put the framerate on screen
+        // and make sure the start time is up to date, do
+        // a reset every 10 seconds
+        if (lMilliStart == 0)
+            lMilliStart = System.currentTimeMillis();
+
+        if ((lMilliNow - lMilliStart) > 10000) {
+            lMilliStart = System.currentTimeMillis();
+            lFrameCount = 0;
+        }
+
+        inputFrame.copyTo(mRgba);
+        sMatSize.width = mRgba.width();
+        sMatSize.height = mRgba.height();
+
+        if (bDisplayTitle) {
+            ShowTitle("RGBA Preview", 1, colorGreen);
+        }
+
+        // get the time now in every frame
+        lMilliNow = System.currentTimeMillis();
+
+        // update the frame counter
+        lFrameCount++;
+
+        if (bDisplayTitle) {
+            string = String.format("FPS: %2.1f", (float)(lFrameCount * 1000) / (float)(lMilliNow - lMilliStart));
+
+            ShowTitle (string, 2, colorGreen);
+        }
+
+        if (bShootNow) {
+            // get the time of the attempt to save a screenshot
+            lMilliShotTime = System.currentTimeMillis();
+            bShootNow = false;
+
+            // try it, and set the screen text accordingly.
+            // this text is shown at the end of each frame until
+            // 1.5 seconds has elapsed
+            if (saveImage(mRgba)) {
+                sShotText = "SCREENSHOT SAVED";
+            }
+            else {
+                sShotText = "SCREENSHOT FAILED";
+            }
+        }
+
+        if (System.currentTimeMillis() - lMilliShotTime < 1500)
+            ShowTitle (sShotText, 3, colorRed);
+
+        return mRgba;
+    }
+
+    public boolean onTouchEvent(final MotionEvent event) {
+        Log.w(TAG, "onTouch!");
+        bShootNow = true;
+        return false;
+    }
+    @SuppressLint("SimpleDateFormat")
+    public boolean saveImage(Mat mat) {
+        Imgproc.cvtColor(mat, mIntermediateMat, Imgproc.COLOR_RGBA2BGR, 3);
+
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        String filename = "OpenCV_";
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        Date date = new Date(System.currentTimeMillis());
+        String dateString = fmt.format(date);
+        filename += dateString + "-" + iFileOrdinal;
+        filename += ".png";
+
+        File file = new File(path, filename);
+
+        Boolean bool;
+        filename = file.toString();
+        bool = Highgui.imwrite(filename, mIntermediateMat);
+
+        return bool;
+    }
+
+    private void ShowTitle (String s, int iLineNum, Scalar color) {
+        Core.putText(mRgba, s, new Point(10, (int)(dTextScaleFactor * 60 * iLineNum)),
+                Core.FONT_HERSHEY_SIMPLEX, dTextScaleFactor, color, 2);
     }
 }
